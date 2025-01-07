49936c5d09f5980f599856c07fbad5a77019068f
Hessx <vitorhesss@gmail.com>
2025-01-07 16:01:16 -0300

    Ajustes finais

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputbox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function AddTask() {
   if (inputbox.value === "") {
     alert("Please enter a task");
@@ -7,5 +8,42 @@ function AddTask() {
     let li = document.createElement("li");
     li.innerHTML = inputbox.value;
     listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+        </svg>
+      `;
+    li.appendChild(span);
+  }
+  inputbox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN" || e.target.closest("span")) {
+      e.target.closest("li").remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  const savedData = localStorage.getItem("data");
+  if (savedData) {
+    listContainer.innerHTML = savedData;
   }
 }
+
+showTask();
